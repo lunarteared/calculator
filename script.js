28400cd1f1cf80976e7c00d33fb3ab87978e94cd
lunarteared <michal.gawlinski02@gmail.com>
2022-09-16 21:49:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 class Calculator {
-    constructor()
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+
+    }
+    
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+
+    }
+    updateDisplay() {
+        this.currentOperandTextElement
+
+
+    }
 }
 
 
@@ -11,3 +45,12 @@ const allClearButton = document.querySelector('[data-all-clear')
 const previousOperandTextElement = document.querySelector('[data-previous-operand')
 const currentOperandTextElement = document.querySelector('[data-current-operand')
 
+const calculator = new Calculator(previousOperandTextElement)
+currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
